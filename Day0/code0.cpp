9eaa75add676c1038c806fe69aa886642b306aaa
Prashant Kumar Jha <itspjprashant07@gmail.com>
2025-07-28 20:13:57 +0530

    arrays 1st day

--- Day0/code0.cpp
+++ Day0/code0.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello, This is Prashant Kumar Jha";
-    return;
+    cout<<"Hello, This is Prashant Kumar Jha \n";
+    cout<<"Prashant \n Kumar \n Jha";
+    return 0;
 }
---
+++ Day1/arrays1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    int marks[5] = {100, 76, 87, 36, 91};
+    double price[5] = {34.5, 65.8, 98.45, 56.8, 54.34};
+
+
+    return 0;
+}
